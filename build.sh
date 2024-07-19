a3765fc389936b9d8504d020483fc534f05af662
cesarmcuellar <43221089+cesarmcuellar@users.noreply.github.com>
2024-07-18 19:28:12 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
-python manage.py collectstatic --no-input
+python ./ProyedctoMesaServicio/manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
+python ./ProyedctoMesaServicio/manage.py migrate
